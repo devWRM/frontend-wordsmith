ab7d063aa8289e1fee183189c9159fed96e32176
devWRM <mcwen@comcast.net>
2021-03-02 10:01:42 -0500

    created container file for subjects

---
+++ src/components/SubjectsContainer.js
@@ -0,0 +1,25 @@
+
+import React, { Component } from 'react';
+
+
+
+
+class SubjectsContainer extends Component {
+    render() {
+
+        return(
+            <div>
+                This is the SubjectsContainer
+            </div>
+        )
+
+
+    }
+
+}
+
+export default SubjectsContainer;
+
+
+
+
