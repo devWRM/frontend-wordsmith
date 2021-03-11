e7b8ee1e666de95dfe60395afda06ce46911dca4
devWRM <mcwen@comcast.net>
2021-03-11 10:13:47 -0500

    imported & displayed the SubjectList component

--- src/components/SubjectsContainer.js
+++ src/components/SubjectsContainer.js
@@ -1,15 +1,26 @@
 
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-
+import { fetchSubjects} from '../actions/subjectActions.js';
+import SubjectList from './SubjectList.js';
 
 
 class SubjectsContainer extends Component {
+
+    componentDidMount() {
+        this.props.fetchSubjects()
+    }
+
     render() {
 
         return(
             <div>
                 This is the SubjectsContainer
+
+                <SubjectList />
+
+
             </div>
         )
 
@@ -18,7 +29,7 @@ class SubjectsContainer extends Component {
 
 }
 
-export default SubjectsContainer;
+export default connect(null, { fetchSubjects })(SubjectsContainer);
 
 
 
