dcd3925aba33c07387728f85d4478f7335c65415
devWRM <mcwen@comcast.net>
2021-03-11 09:14:04 -0500

    tested store with console.log

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
-
+import { rootReducer } from './reducers/rootReducer.js';
 import './index.css';
-import App from './components/App';
+import App from './components/App.js';
 
 
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
 )
-
+console.log(store)
 
 ReactDOM.render(
   <Provider store={store}>
