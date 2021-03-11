fc4297c62a49bc2ec9fdf10d6f747e6fc36d0178
devWRM <mcwen@comcast.net>
2021-03-11 09:58:05 -0500

    used dispatch to populate the subjects store

--- src/actions/subjectActions.js
+++ src/actions/subjectActions.js
@@ -2,6 +2,7 @@ export const fetchSubjects = () => {
     return (dispatch) => {
         fetch('http://localhost:3000/subjects')
         .then(resp => resp.json())
-        .then(subjects => console.log('fetchSubjects', subjects))
+        .then(subjects => dispatch({ type: 'FETCH_SUBJECTS', payload: subjects }))
+        // .then(subjects => console.log('fetchSubjects', subjects))
     }
 }
