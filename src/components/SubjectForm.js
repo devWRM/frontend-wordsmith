3b60cab1ad8cce86def1d7b6e2d0a2e5877fc87f
devWRM <mcwen@comcast.net>
2021-03-12 21:50:03 -0500

    created state for name

--- src/components/SubjectForm.js
+++ src/components/SubjectForm.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react'
 
 class SubjectForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: ""
+        }
+    }
+
+
     render() {
         return (
             <div>
