77e8f660899f220c72c8a58b90653b96ee46f828
devWRM <mcwen@comcast.net>
2021-03-12 21:45:06 -0500

    created student form component

---
+++ src/components/SubjectForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SubjectForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default SubjectForm;
