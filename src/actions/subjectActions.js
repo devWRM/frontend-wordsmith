e5a28680483f2e04454414bd9d299299acc95fd5
devWRM <mcwen@comcast.net>
2021-03-14 11:17:41 -0400

    built deleteSubject function

--- src/actions/subjectActions.js
+++ src/actions/subjectActions.js
@@ -19,6 +19,17 @@ export const newSubject = (subjectInput) => {
     }
 }
 
+export function deleteSubject(subjectID) {
+    return (dispatch) => {
+        fetch(`http://localhost:3000/subjects/${subjectID}`,{
+            method: 'DELETE',
+            body: JSON.stringify(subjectID),
+            headers: { 'Content-Type': 'application/json'}    
+        })
+        .then(resp => resp.json())
+        .then(subjectData => dispatch({ type: 'DELETE_SUBJECT', payload: subjectData }))
+    }
+}
 
 
 
