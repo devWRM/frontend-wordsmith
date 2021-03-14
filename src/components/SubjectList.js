6181f72f164ea368c80049f1970e4d6b674ac8fb
devWRM <mcwen@comcast.net>
2021-03-14 11:21:03 -0400

    built delete button for subject & connected functionality with import deleteSubject & mapped dispatch

--- src/components/SubjectList.js
+++ src/components/SubjectList.js
@@ -1,13 +1,19 @@
 import React from 'react';
-
 import { connect } from 'react-redux';
+
+import { deleteSubject } from '../actions/subjectActions.js';
+
 
 function SubjectList(props) {
     return (
         <div>
             SubjectList<br></br>
             { props.subjects.length ? props.subjects.map(subject => 
-                <li>{subject.name}</li>         
+                <li key={subject.id}>
+                    <button onClick={ () => props.deleteSubject(subject.id)}>delete {subject.name}</button>
+
+                    {subject.name}                  
+                </li>         
             )
              : <p>Add a subject to begin.</p>}
         </div>
@@ -18,4 +24,10 @@ const mapStateToProps = (state) => {
     return { subjects: state.subjects }
 }
 
-export default connect(mapStateToProps)(SubjectList);
+const mapDispatchToProps = dispatch => {
+    return {
+            deleteSubject: subjectID => dispatch(deleteSubject(subjectID))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SubjectList);
