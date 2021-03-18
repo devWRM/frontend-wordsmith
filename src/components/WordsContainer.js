2f83ec81c667ca043e627f3ac643e4cfd5243163
devWRM <mcwen@comcast.net>
2021-03-18 19:56:39 -0400

    refactored code to import & render words props passed down to WordList

--- src/components/WordsContainer.js
+++ src/components/WordsContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import WordForm from './WordForm.js';
+import WordList from './WordList.js';
 
 class WordsContainer extends Component {
 
@@ -11,13 +12,15 @@ class WordsContainer extends Component {
 
                 <WordForm subject={this.props.subject}/>
                 
-                {this.props.subject.words.map(word =>
+                <WordList words={this.props.subject.words}/>
+
+                {/* {this.props.subject.words.map(word =>
                     <div key={word.id}>
                         <p>{word.spelling}</p>
 
                     </div>
 
-                )}
+                )} */}
             </div>
         )
     }
