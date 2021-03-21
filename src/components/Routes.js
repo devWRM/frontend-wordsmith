442d00c59286a52bfb040d7b618d8886e9c92d9c
devWRM <mcwen@comcast.net>
2021-03-20 21:14:28 -0400

    created route for Resources

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import Home from './Home.js';
 import About from './About.js';
+import Resources from './Resources.js';
 import SubjectList from './SubjectList.js';
 import Subject from './Subject.js';
 import Word from './Word.js';
@@ -14,6 +15,7 @@ function Routes() {
             <Switch>
                 <Route exact path='/' component={Home} />
                 <Route path='/about' component={About} />
+                <Route path='/resources' component={Resources} />
                 <Route exact path='/subjects' component={SubjectList} />
                 <Route path='/subjects/:id' component={Subject} />
                 <Route path='/subjects/:id/words/:id' component={Word} />
