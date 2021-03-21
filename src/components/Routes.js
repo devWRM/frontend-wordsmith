d855aa149dd7e5247a9df7cc0d25c19d0ffb81b1
devWRM <mcwen@comcast.net>
2021-03-20 21:10:22 -0400

    created routes for Home, About, SubjectList, Subject, & WOrd

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-
+import Home from './Home.js';
+import About from './About.js';
+import SubjectList from './SubjectList.js';
+import Subject from './Subject.js';
+import Word from './Word.js';
 
 function Routes() {
     return (
         <div>
-            
+            ROUTES HERE
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/about' component={About} />
+                <Route exact path='/subjects' component={SubjectList} />
+                <Route path='/subjects/:id' component={Subject} />
+                <Route path='/subjects/:id/words/:id' component={Word} />
+            </Switch>
+
+
         </div>
     )
 }
