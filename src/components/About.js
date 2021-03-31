49c184ea53d145bd4c3766c044654c3f8a649949
devWRM <mcwen@comcast.net>
2021-03-30 22:50:05 -0400

    added text on what users can do with the app

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,19 @@ import React from 'react'
 function About() {
     return (
         <div className="container">
-            ABOUT
+            <h3>WORDSMITH</h3>
+            <p>Track your English words to increase your vocabulary and improve your communication skills.</p>
+            <ul>
+                <li>see a list of subjects</li>
+                <li>create a subject</li>
+                <li>delete a subject (including all of its words)</li>
+                <li>see a list of words for a specific subject</li>
+                <li>see a words spelling, part of speech, definition, and sentences</li>
+                <li>create a word for a specific subject</li>
+                <li>delete a subject</li>
+            </ul>
+            
+
         </div>
     )
 }
