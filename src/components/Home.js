2fe253b3be73d6881e0ab318c61136ae4747967b
devWRM <mcwen@comcast.net>
2021-03-31 13:46:08 -0400

    edited text with definition of wordsmith

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,15 @@ import React from 'react'
 function Home() {
     return (
         <div className="container">
-            Welcome to Wordsmith: Track your English words to increase your vocabulary and improve your communication skills.
+            <h2>Welcome to Wordsmith</h2>
+            <h4>wordsmith [ wurd-smith ] <i>noun</i>
+                <br></br>- an expert in the use of words
+                <br></br>- special skill or knowledge in word usage
+                {/* <br></br>- a collector of words
+                <br></br>- skilled in the use of words
+                <br></br>- an authority in word usage */}
+
+            </h4>
         </div>
     )
 }
