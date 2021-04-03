c69f5bb4673bf4452cbd2ca13d377487fd82869e
devWRM <mcwen@comcast.net>
2021-04-03 14:35:18 -0400

    edited text

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,14 @@ function Home() {
     return (
         <div className="container">
             <h2>Welcome to Wordsmith</h2>
-            <h4>wordsmith [ wurd-smith ] <i>noun</i>
-                <br></br>- an expert in the use of words
-                <br></br>- special skill or knowledge in word usage
+            <h4>wordsmith [ wurd-smith ] <i>noun</i></h4>
+                - an expert in the use of words<br></br>
+                - special skill in word usage
                 {/* <br></br>- a collector of words
                 <br></br>- skilled in the use of words
                 <br></br>- an authority in word usage */}
 
-            </h4>
+            
         </div>
     )
 }
