5bcc06d703c74cc26d2da985a98d8a9864b9406b
devWRM <mcwen@comcast.net>
2021-04-08 21:45:34 -0400

    added resources links

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -2,8 +2,42 @@ import React from 'react'
 
 function Resources() {
     return (
-        <div>
-            RESOURCES
+        <div className="container">
+            <h2>RESOURCES</h2>
+            <br></br>
+            <p><a
+                className="resources-link"
+                href="https://translate.google.com"
+                target="_blank"
+                // rel="noopener noreferrer"
+            >
+                  Google Translate
+            </a></p>
+
+            <p><a
+                className="resources-link"
+                href="https://www.youtube.com/watch?v=gaqYzeHU8sI"
+                target="_blank"
+            >
+                English Practice 1
+            </a></p>
+
+            <p><a
+                className="resources-link"
+                href="https://www.youtube.com/watch?v=ZmUm29fc6nI"
+                target="_blank"
+            >
+                English Practice 2
+            </a></p>
+
+            <p><a
+                // className="resources-link"
+                href="https://www.youtube.com/watch?v=XiA6BdwLrG4"
+                target="_blank"
+            >
+                English Practice 3
+            </a></p>
+
         </div>
     )
 }
