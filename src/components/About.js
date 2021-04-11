8a0f7633e00aafd873f45cc166cec7f80b4a8ac9
devWRM <mcwen@comcast.net>
2021-04-11 18:39:25 -0400

    edited About text to describe app use

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <div className="container">
             <h2>ABOUT WORDSMITH</h2>
-            <p>Track your English words to increase your vocabulary and improve your communication skills.</p>
+            <p>Track your English words to increase your vocabulary and improve your communication skills. Study the words of a subject before an event so you're prepared to understand what is spoken and also speak clearly and confidently on that subject. For example, before going to the bank, review the bank subject so you're familiar with words associated with banking. Then you'll be prepared to clearly communicate your banking needs.</p>
             <ul>
                 <li>see a list of subjects</li>
                 <li>create a subject</li>
