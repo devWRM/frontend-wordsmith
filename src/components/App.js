8d42d0a39691e0b4b969cc85ed7167a3d55c7342
devWRM <mcwen@comcast.net>
2021-04-19 20:20:24 -0400

    deleted obsolete code

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import Routes from './Routes.js';
 
 
 import './App.css';
@@ -12,27 +11,18 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <p>
             WORDSMITH
           </p>
 
             <NavBar />
 
-              {/* <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a> */}
+              
         </header>
 
               <div className="app-container">
 
                 <SubjectsContainer />
-              {/* <Routes /> */}
 
               </div>
 
