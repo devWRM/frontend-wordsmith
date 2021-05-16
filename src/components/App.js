a5d0cfd5258654689c13c21860073200f1f8af3f
devWRM <mcwen@comcast.net>
2021-05-16 14:39:10 -0400

    imported & rendered User in the header

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import './App.css';
 import NavBar from './NavBar';
 import SubjectsContainer from './SubjectsContainer';
+import User from './User.js';
 
 
 class App extends React.Component {
@@ -15,12 +16,15 @@ class App extends React.Component {
             WORDSMITH
           </p>
 
+            <User />
             <NavBar />
 
               
         </header>
 
               <div className="app-container">
+
+               
 
                 <SubjectsContainer />
 
