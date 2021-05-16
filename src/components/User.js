a91bb716e3a68f6a6f04d8d562d747ac90c08e80
devWRM <mcwen@comcast.net>
2021-05-16 14:38:22 -0400

    created a User class component

---
+++ src/components/User.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+
+
+class User extends React.Component {
+
+    render() {
+
+        return(
+            <div>
+                STILL A USER
+            </div>
+        )
+
+    }
+
+
+
+}
+
+export default User;
