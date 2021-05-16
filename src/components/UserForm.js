da55cd5920e1d6ac268a1f710c5fa77de293b2a9
devWRM <mcwen@comcast.net>
2021-05-16 17:19:10 -0400

    created a class UserForm & imported connect

---
+++ src/components/UserForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+
+class UserForm extends React.Component {
+
+    render() {
+        return(
+            <div>
+                Fill out this User form.
+            </div>
+        )
+    }
+
+
+
+}
+
+
+export default UserForm;
